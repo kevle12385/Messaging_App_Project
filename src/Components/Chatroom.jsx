584c6e6f63d092eb35a6d7624347424fdd514e31
kevle12385 <kevle12385@ymail.com>
2024-04-03 01:59:34 -0700

    ccccccc

--- src/Components/Chatroom.jsx
+++ src/Components/Chatroom.jsx
@@ -45,7 +45,7 @@ function Chatroom({selectedId, foundMessages}) {
       // Initialize or reuse the socket connection
       if (!socketRef.current) {
         console.log("Initializing new socket connection");
-        let socketServerURL = "wss://nodejs-production-49b7.up.railway.app";
+        let socketServerURL = "https://nodejs-production-49b7.up.railway.app";
         socketRef.current = io(socketServerURL, {
           transports: ["websocket", "polling"],
           query: { chatId: selectedId, userId: currentUserID },
